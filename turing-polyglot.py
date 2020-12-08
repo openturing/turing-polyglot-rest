020190b848c425bdbd8da01424c3c67fae742f09
Alexandre Oliveira <alexandre.oliveira@viglet.com>
2020-12-08 11:22:17 -0300

    - Names and passport

--- turing-polyglot.py
+++ turing-polyglot.py
@@ -5,7 +5,8 @@ import hug
 from hug_middleware_cors import CORSMiddleware
 from polyglot.text import Text
 import re
-
+from nameparser import HumanName
+import json
 
 @hug.post('/ent')
 def ent(text: str, model: str):
@@ -13,20 +14,27 @@ def ent(text: str, model: str):
 
     emails = re.findall(r'[\w\.-]+@[\w\.-]+', text)
 
+    passport_regex = "[A-PR-WYa-pr-wy][1-9]\\d" + "\\s?\\d{4}[1-9]"
+    passport_p = re.compile(passport_regex)
+
+    passport = re.findall(passport_p, text)
     polyglot_text = Text(text,  hint_language_code=model)
 
     places = []
     persons = []
+    names = []
     orgs = []
     for entity in polyglot_text.entities:
         if entity.tag == 'I-LOC':
             places.append(' '.join(entity))
         if entity.tag == 'I-PER':
-            persons.append(' '.join(entity))
+            person = ' '.join(entity).replace(" .",".")
+            persons.append(person)
+            names.append(HumanName(person).as_dict())
         if entity.tag == 'I-ORG':
             orgs.append(' '.join(entity))
 
-    return [{'places': places, 'persons': persons, 'orgs': orgs, 'emails': emails}]
+    return [{'places': places, 'persons': persons, 'orgs': orgs, 'emails': emails, 'passport': passport, 'names': names}]
 
 
 if __name__ == '__main__':
