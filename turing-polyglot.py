6227e0ac46dd46a802a0106183d7040164a01a3d
Alexandre Oliveira <alexandre.oliveira@viglet.com>
2020-12-08 12:07:34 -0300

    - CIF

--- turing-polyglot.py
+++ turing-polyglot.py
@@ -8,6 +8,7 @@ import re
 from nameparser import HumanName
 import json
 
+
 @hug.post('/ent')
 def ent(text: str, model: str):
     """Get entities for polyglot Entities."""
@@ -19,18 +20,20 @@ def ent(text: str, model: str):
     passport = re.findall(passport_p, text)
 
     phone_regex = r"\b[0-9]{2,3}-? ?[0-9]{6,7}\b"
-    phone_p = re.compile(phone_regex)   
+    phone_p = re.compile(phone_regex)
     phones = re.findall(phone_p, text)
 
     dni_regex = r"\b[0-9]{8,8}[A-Za-z]\b"
-    dni_p = re.compile(dni_regex)   
+    dni_p = re.compile(dni_regex)
     dnis = re.findall(dni_p, text)
 
     nie_regex = r"\b[XYZ][0-9]{7,8}[A-Z]\b"
-    #nie_regex = "[XYZ][0-9]{7,8}[A-Z]"
-    nie_p = re.compile(nie_regex)   
+    nie_p = re.compile(nie_regex)
     nies = re.findall(nie_p, text)
 
+    cif_regex = r"\b[ABCDEFGHJKLMNPQRSUVW][0-9]{7}[0-9A-J]\b"
+    cif_p = re.compile(cif_regex)
+    cifs = re.findall(cif_p, text)
     polyglot_text = Text(text,  hint_language_code=model)
 
     places = []
@@ -41,13 +44,13 @@ def ent(text: str, model: str):
         if entity.tag == 'I-LOC':
             places.append(' '.join(entity))
         if entity.tag == 'I-PER':
-            person = ' '.join(entity).replace(" .",".")
+            person = ' '.join(entity).replace(" .", ".")
             persons.append(person)
             names.append(HumanName(person).as_dict())
         if entity.tag == 'I-ORG':
             orgs.append(' '.join(entity))
 
-    return [{'places': places, 'persons': persons, 'orgs': orgs, 'emails': emails, 'passport': passport, 'names': names, 'phones': phones, 'dni': dnis, 'nie': nies}]
+    return [{'places': places, 'persons': persons, 'orgs': orgs, 'emails': emails, 'passport': passport, 'names': names, 'phones': phones, 'dni': dnis, 'nie': nies, 'cif': cifs}]
 
 
 if __name__ == '__main__':
